e0f463dc0065fc5c6215a68b374396a6c224f105
mdanialr <mdanialrma@gmail.com>
2022-01-26 21:07:49 +0700

    add test for Default Error Handler

---
+++ internal/handlers/default_error_test.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"errors"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultError(t *testing.T) {
+	testCases := []struct {
+		name         string
+		route        string
+		expectedCode int
+		expectedMIME string
+	}{
+		{
+			name:         "GET /err : internal server error should return 500 in JSON response",
+			route:        "/err",
+			expectedCode: 500,
+			expectedMIME: fiber.MIMEApplicationJSON,
+		},
+		{
+			name:         "GET /error : internal server error should return 500 in JSON response",
+			route:        "/error",
+			expectedCode: 500,
+			expectedMIME: fiber.MIMEApplicationJSON,
+		},
+	}
+
+	app := fiber.New(fiber.Config{ErrorHandler: DefaultError})
+	app.Get("/err", func(c *fiber.Ctx) error {
+		// This should trigger default error handler
+		return fiber.ErrInternalServerError
+	})
+	app.Get("/error", func(c *fiber.Ctx) error {
+		return errors.New("this should trigger type assertion error")
+	})
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(fiber.MethodGet, tt.route, nil)
+
+			res, _ := app.Test(req)
+
+			assert.Equal(t, tt.expectedCode, res.StatusCode)
+			assert.Equal(t, tt.expectedMIME, res.Header.Get("Content-Type"))
+		})
+	}
+}

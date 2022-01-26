df00ddd97a16691ea98e3ebc1df30de84fd54616
mdanialr <mdanialrma@gmail.com>
2022-01-26 19:34:22 +0700

    rename to more suitable name

--- internal/handlers/not_found.go
+++ internal/handlers/not_found.go
@@ -2,9 +2,9 @@ package handlers
 
 import "github.com/gofiber/fiber/v2"
 
-// DefaultRouteFound default handler to catch
+// DefaultRouteNotFound default handler to catch
 // all not found error then return json message.
-func DefaultRouteFound(c *fiber.Ctx) error {
+func DefaultRouteNotFound(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 		"message": "oops!! route not found",
 	})

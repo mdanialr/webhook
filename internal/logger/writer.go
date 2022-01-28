be9b8193581bd53b0a416a31e46d8ff9d591db9c
mdanialr <mdanialrma@gmail.com>
2022-01-29 03:52:57 +0700

    move internal logger to their own pkg w test

---
+++ internal/logger/writer.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mdanialr/webhook/internal/config"
+)
+
+var (
+	InfL *log.Logger
+	ErrL *log.Logger
+)
+
+// InitLogger init and setup log file to write internal logger for this app.
+func InitLogger(conf *config.Model) error {
+	fl, err := os.OpenFile(conf.LogDir+"app-log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0770)
+	if err != nil {
+		return fmt.Errorf("failed to open|create log file: %v", err)
+	}
+
+	InfL = log.New(fl, "[INFO] ", log.Ldate|log.Ltime)
+	ErrL = log.New(fl, "[ERROR] ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	return nil
+}
---
+++ internal/logger/writer_test.go
@@ -0,0 +1,37 @@
+package logger
+
+import (
+	"github.com/mdanialr/webhook/internal/config"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestInitLogger(t *testing.T) {
+	testCases := []struct {
+		name       string
+		sampleConf config.Model
+		isErr      bool
+	}{
+		{
+			name: "Success w no error",
+			sampleConf: config.Model{
+				LogDir: "/tmp/",
+			},
+			isErr: false,
+		},
+		{name: "Failed w error", isErr: true},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.sampleConf.SanitizationLog()
+
+			switch tt.isErr {
+			case false:
+				require.NoError(t, InitLogger(&tt.sampleConf))
+			case true:
+				require.Error(t, InitLogger(&tt.sampleConf))
+			}
+		})
+	}
+}

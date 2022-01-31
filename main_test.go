000ca15f09d4e5c9782a53ccfe7ceb916dcbaee8
mdanialr <mdanialrma@gmail.com>
2022-01-31 17:21:31 +0700

    move one test to bottom and use their own data seeder which is fake log dir

--- main_test.go
+++ main_test.go
@@ -53,6 +53,15 @@ service:
 		require.NoError(t, err)
 	})
 
+	t.Run("Success must exactly the same as in config file", func(t *testing.T) {
+		_, err := setup(&appConf, bytes.NewBufferString(fakeConfigFile))
+		require.NoError(t, err)
+
+		assert.Equal(t, "localhost", appConf.Host)
+		assert.Equal(t, strconv.Itoa(int(uint16(5454))), strconv.Itoa(int(appConf.PortNum)))
+		assert.Equal(t, "/home/nzk/test-app/webhook/log/", appConf.LogDir)
+	})
+
 	fakeConfigFile =
 		`
 env: prod
@@ -61,6 +70,18 @@ log: /tmp
 `
 
 	t.Run("Secret does not exist on config file should return error", func(t *testing.T) {
+		_, err := setup(&appConf, bytes.NewBufferString(fakeConfigFile))
+		require.Error(t, err)
+	})
+
+	fakeConfigFile =
+		`
+env: prod
+port: 5050
+log: /fake/dir
+`
+
+	t.Run("Log dir does not exist should return error", func(t *testing.T) {
 		_, err := setup(&appConf, bytes.NewBufferString(fakeConfigFile))
 		require.Error(t, err)
 	})

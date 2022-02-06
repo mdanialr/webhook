85724ae7b8a454643ead2e73365f4a5953a65d05
mdanialr <mdanialrma@gmail.com>
2022-02-07 00:16:14 +0700

    remove verification phase, just do CD

--- internal/handlers/hook.go
+++ internal/handlers/hook.go
@@ -37,48 +37,19 @@ func Hook(conf *config.Model, jobC chan string) func(*fiber.Ctx) error {
 			})
 		}
 
-		confKeyword := conf.Keyword
-		confUsr := conf.Usr
-
 		remoteUsr := reqHook.Commits[0].Committer.Username
 		message := reqHook.Commits[0].Message
 		branch := strings.Split(reqHook.Branch, "/")
 		branchName := strings.Join(branch[len(branch)-1:], "")
 
-		// 1# Validate config's username against incoming committer.
-		// 2# Validate config's keyword against incoming message's prefix.
-		// 3# If both config's username or keyword is 'empty' no need to
-		// validate username.
-
-		var isReload bool
-		switch {
-		case confUsr == remoteUsr && confKeyword == "empty":
-			isReload = true
-		case confUsr == "empty" && strings.HasPrefix(message, confKeyword):
-			isReload = true
-		case confUsr == "empty" && confKeyword == "empty":
-			isReload = true
-		case confUsr == remoteUsr && strings.HasPrefix(message, confKeyword):
-			isReload = true
-		case confUsr != remoteUsr && confKeyword == "empty":
-			isReload = false
-		case confUsr == remoteUsr && !strings.HasPrefix(message, confKeyword):
-			isReload = false
-		case confUsr == "empty" && !strings.HasPrefix(message, confKeyword):
-			isReload = false
-		}
-
-		if isReload {
-			go func() {
-				jobC <- repo
-			}()
-		}
+		go func() {
+			jobC <- repo
+		}()
 
 		return c.JSON(fiber.Map{
 			"committer": remoteUsr,
 			"message":   message,
 			"branch":    branchName,
-			"reload":    isReload,
 			"repo":      repo,
 		})
 	}

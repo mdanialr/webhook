c1c180e3604d48b563715616544243686f527435
mdanialr <mdanialrma@gmail.com>
2022-08-07 19:17:52 +0700

    add new request model and ID creation's implementation along with the test

--- internal/model/request_payload_test.go
+++ internal/model/request_payload_test.go
@@ -1,4 +1,4 @@
-package github
+package model
 
 import (
 	"testing"
@@ -15,28 +15,28 @@ func TestRequestPayload_CreateId(t *testing.T) {
 		{
 			name:   "1# Result should be as expected",
 			sample: RequestPayload{Repo: "user/repo", Ref: "refs/heads/main"},
-			expect: RequestPayload{Repo: "user/repo", Ref: "refs/heads/main", User: "user", RepoName: "repo", Branch: "main", Id: "user_repo_main"},
+			expect: RequestPayload{Repo: "user/repo", Ref: "refs/heads/main", User: "user", RepoName: "repo", Branch: "main", ID: "user_repo_main"},
 		},
 		{
 			name:   "2# Result should be as expected",
 			sample: RequestPayload{Repo: "us/repo-y", Ref: "refs/heads/stable"},
-			expect: RequestPayload{Repo: "us/repo-y", Ref: "refs/heads/stable", User: "us", RepoName: "repo-y", Branch: "stable", Id: "us_repo-y_stable"},
+			expect: RequestPayload{Repo: "us/repo-y", Ref: "refs/heads/stable", User: "us", RepoName: "repo-y", Branch: "stable", ID: "us_repo-y_stable"},
 		},
 		{
 			name:   "1# Should use branch `tags` instead if the payload is a tag",
 			sample: RequestPayload{Repo: "user/repo", Ref: "refs/tags/v1.0"},
-			expect: RequestPayload{Repo: "user/repo", Ref: "refs/tags/v1.0", User: "user", RepoName: "repo", Branch: "tags", Id: "user_repo_tags"},
+			expect: RequestPayload{Repo: "user/repo", Ref: "refs/tags/v1.0", User: "user", RepoName: "repo", Branch: "v1.0", ID: "user_repo_tags", Tags: true},
 		},
 		{
 			name:   "2# Should use the branch `tags` instead if the payload is a tag",
 			sample: RequestPayload{Repo: "user/repo", Ref: "refs/tags/v2.4"},
-			expect: RequestPayload{Repo: "user/repo", Ref: "refs/tags/v2.4", User: "user", RepoName: "repo", Branch: "tags", Id: "user_repo_tags"},
+			expect: RequestPayload{Repo: "user/repo", Ref: "refs/tags/v2.4", User: "user", RepoName: "repo", Branch: "v2.4", ID: "user_repo_tags", Tags: true},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.sample.CreateId()
+			tc.sample.CreateID()
 			assert.Equal(t, tc.expect, tc.sample)
 		})
 	}

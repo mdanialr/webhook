2d9e3743502a09d232269d721a5034d771a0cab7
mdanialr <mdanialrma@gmail.com>
2022-08-07 19:18:25 +0700

    ref: drop some endpoint and only support one endpoint

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,50 +1,20 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/mdanialr/webhook/internal/config"
 	"github.com/mdanialr/webhook/internal/handlers"
-	nzLog "github.com/mdanialr/webhook/internal/logger"
 	"github.com/mdanialr/webhook/internal/middlewares"
+	"github.com/mdanialr/webhook/internal/model"
 	"github.com/mdanialr/webhook/internal/worker"
+	"github.com/mdanialr/webhook/pkg/config"
 )
 
-func SetupRoutes(app *fiber.App, conf *config.Model, l nzLog.Interface, bag worker.BagOfChannels, hCl *http.Client) {
-	// Built-in fiber middlewares
-	app.Use(recover.New())
-	// Use log file only in production
-	switch conf.EnvIsProd {
-	case true:
-		fConf := logger.Config{
-			Format:     "[${time}] ${status} | ${method} - ${latency} - ${ip} | ${path}\n",
-			TimeFormat: "02-Jan-2006 15:04:05",
-			Output:     conf.LogFile,
-		}
-		app.Use(logger.New(fConf))
-	case false:
-		app.Use(logger.New())
-	}
-
-	// This app's endpoints
+func SetupRoutes(app *fiber.App, appConf *config.AppConfig, bag worker.BagOfChannels, svc *model.Service) {
 	app.Get("/", handlers.Home)
-	app.Post("/hook/:repo",
-		middlewares.ReloadConfig(conf, l),
-		middlewares.SecretToken(conf),
-		handlers.GithubWebhook(bag.GithubWebhookChan.JobC),
-	)
-	app.Post("/docker/webhook",
-		middlewares.ReloadConfig(conf, l),
-		handlers.DockerHubWebhook(bag.DockerWebhookChan.JobC, hCl),
-	)
 	app.Post("/github/webhook",
-		middlewares.SecretToken(conf),
-		handlers.GithubAction(bag.GithubActionChan.JobC),
+		middlewares.Auth(appConf),
+		handlers.GithubAction(bag.GithubActionChan.JobC, svc),
 	)
 
-	// Custom middlewares AFTER endpoints
 	app.Use(handlers.DefaultRouteNotFound)
 }
--- internal/routes/routes_test.go
+++ internal/routes/routes_test.go
@@ -1,38 +1,22 @@
 package routes
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/mdanialr/webhook/internal/config"
+	"github.com/mdanialr/webhook/internal/model"
 	"github.com/mdanialr/webhook/internal/worker"
+	"github.com/mdanialr/webhook/pkg/config"
 )
 
-type fakeLogger struct{}
-
-func (f *fakeLogger) Println(_ ...interface{}) {}
-
 func TestSetupRoutes(t *testing.T) {
-	var fakeServer = httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {}))
 	bags := worker.BagOfChannels{
-		GithubActionChan:  &worker.Channel{JobC: make(chan string)},
-		GithubWebhookChan: &worker.Channel{JobC: make(chan string)},
-		DockerWebhookChan: &worker.Channel{JobC: make(chan string)},
+		GithubActionChan: &worker.Channel{JobC: make(chan string)},
 	}
 
-	t.Run("1# Success test", func(t *testing.T) {
-		conf := config.Model{Secret: "1"}
+	t.Run("Should pass", func(t *testing.T) {
 		app := fiber.New()
 
-		SetupRoutes(app, &conf, &fakeLogger{}, bags, fakeServer.Client())
-	})
-
-	t.Run("2# Success test", func(t *testing.T) {
-		conf := config.Model{Secret: "1", EnvIsProd: true}
-		app := fiber.New()
-
-		SetupRoutes(app, &conf, &fakeLogger{}, bags, fakeServer.Client())
+		SetupRoutes(app, &config.AppConfig{}, bags, &model.Service{})
 	})
 }
